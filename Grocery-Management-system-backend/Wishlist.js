79f14ec7cd4df2772de71e6986a8c875954a74a0
R Kanishkasree <124056440+KanishkasreeR@users.noreply.github.com>
2024-08-17 13:40:40 +0530

    Wishlist.js

--- Grocery-Management-system-backend/Wishlist.js
+++ Grocery-Management-system-backend/Wishlist.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 
-// Define the schema for the Wishlist
 const wishlistSchema = new mongoose.Schema({
   customerId: {
     type: String,
   },
   products: [{
-    type: String, // Reference to the Product model
+    type: String, 
   }],
   adminId:{
     type: String,
   }
 });
 
-// Create the Wishlist model
 const Wishlist = mongoose.model('Wishlist', wishlistSchema);
 
 module.exports = Wishlist;
