011a76e23a17e7383523e23871b927451eb058bf
R Kanishkasree <124056440+KanishkasreeR@users.noreply.github.com>
2024-08-17 13:38:52 +0530

    Cart.js

--- Grocery-Management-system-backend/Cart.js
+++ Grocery-Management-system-backend/Cart.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 
-// Define the schema for the Wishlist
 const CartSchema = new mongoose.Schema({
   customerId: {
     type: String,
   },
   products: [{
-    type: String, // Reference to the Product model
+    type: String, 
   }],
   adminId:{
     type: String,
   }
 });
 
-// Create the Wishlist model
 const Cart = mongoose.model('Cart', CartSchema);
 
 module.exports = Cart;
