1a5f735ea85e8cffc97dfbead81c8ef6b59e67ed
R Kanishkasree <124056440+KanishkasreeR@users.noreply.github.com>
2024-08-17 13:38:32 +0530

    Adminmodel.js

--- Grocery-Management-system-backend/Adminmodel.js
+++ Grocery-Management-system-backend/Adminmodel.js
@@ -1,58 +1,4 @@
-// const mongoose = require("mongoose");
-
-// const AdminSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, "Name is required."],
-//   },
-//   email: {
-//     type: String,
-//     required: [true, "Email is required."],
-//     unique: true, // Ensures uniqueness of email addresses
-//   },
-//   password: {
-//     type: String,
-//     required: [true, "Password is required."],
-//   }
-// });
-
-// const Admin = mongoose.model("Admin", AdminSchema);
-
-// module.exports = Admin;
-
 const mongoose = require("mongoose");
-
-// const AdminSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, "Name is required."],
-//   },
-//   email: {
-//     type: String,
-//     required: [true, "Email is required."],
-//     unique: true, // Ensures uniqueness of email addresses
-//   },
-//   password: {
-//     type: String,
-//     required: [true, "Password is required."],
-//   },
-//   storeName: {
-//     type: String,
-//     required: [true, "Store name is required."],
-//   },
-//   storeAddress: {
-//     type: String,
-//     required: [true, "Store address is required."],
-//   },
-//   contactNumber: {
-//     type: String,
-//     required: [true, "Contact number is required."],
-//   },
-// });
-
-// const Admin = mongoose.model("Admin", AdminSchema);
-
-// module.exports = Admin;
 
 const AdminSchema = new mongoose.Schema({
   name: {
@@ -62,7 +8,7 @@ const AdminSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Email is required."],
-    unique: true, // Ensures uniqueness of email addresses
+    unique: true,
   },
   password: {
     type: String,
@@ -79,14 +25,14 @@ const AdminSchema = new mongoose.Schema({
   contactNumber: {
     type: String,
     required: [true, "Contact number is required."],
-    unique: true, // Ensures uniqueness of contact numbers
+    unique: true, 
   },
   storeHours: {
     type: String,
     required: [true, "Store hours are required."],
   },
   storeImage: {
-    type: String, // Assuming you'll store the image URL
+    type: String,
     required: [true, "Store image is required."],
   },resetPasswordToken: {
     type: String,
